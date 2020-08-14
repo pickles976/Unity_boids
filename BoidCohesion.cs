ea8c10b48c0ec3841367766fc548be12a823d665
Juan Gomez <sebaslogo@gmail.com>
2020-08-13 19:22:07 -0500

    Add files via upload

---
+++ BoidCohesion.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidCohesion : AgentBehavior
+{
+    public float neighborDist = 15.0f;
+    public List<GameObject> targets;
+
+    public override steering GetSteering()
+    {
+        steering steer = new steering();
+        int count = 0;
+
+        foreach(GameObject other in targets)
+        {
+            if(other != null)
+            {
+                float d = (transform.position - other.transform.position).magnitude;
+                if((d > 0) && (d < neighborDist))
+                {
+                    steer.linear += other.transform.position;
+                    count++;
+                }
+            }
+        }//endfor
+
+        if (count > 0)
+        {
+            steer.linear /= count;
+            steer.linear = steer.linear - transform.position;
+        }
+
+        return steer;
+    }
+}
---
+++ BoidSeparation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidSeparation : Flee
+{
+    public float desiredSeparation = 15.0f;
+    public List<GameObject> targets;
+
+    public override steering GetSteering()
+    {
+        steering steer = new steering();
+        int count = 0;
+
+        //for each boid in the system, check if it is too close
+        foreach(GameObject other in targets)
+        {
+            if (other != null)
+            {
+                float d = (transform.position - other.transform.position).magnitude;
+
+                if((d > 0) && (d < desiredSeparation))
+                {
+                    //calculate vector pointing away from neighbor
+                    Vector3 diff = transform.position - other.transform.position;
+                    diff.Normalize();
+                    diff /= d;
+                    steer.linear += diff;
+                    count++;
+                }
+            }
+        }//end for
+
+        if(count > 0)
+        {
+            //steer.linear /= (float)count;
+        }
+
+        return steer;
+
+    }
+}
